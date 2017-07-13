af59fb1efb03b12cc6fbc5f2a12064034ee4bd4f
santiandres33 <santiandres33@hotmail.com>
2017-07-13 18:33:15 -0500

    updating dices, olympcis, garden, racer

--- challenges/3-Dices/application.js
+++ challenges/3-Dices/application.js
@@ -1,16 +1,99 @@
-$(document).ready(function() {  // Codigo Procedural Procedimental --> OOP
-  // Event Listener
-  $('#roller button.add').on('click', function() { // Controlador
-    $('.dice').append('<div class="die">0</div>'); // Vista
-  });
+// $(document).ready(function() {  // Codigo Procedural Procedimental --> OOP
+//   // Event Listener
+//   $('#roller button.add').on('click', function() { // Controlador
+//     $('.dice').append('<div class="die">0</div>'); // Vista
+//   });
 
-  // Event listener
-  $('#roller button.roll').on('click', function() { // Controlador
-    $('.die').each(function(index, dieHtml) { // Controlador
-      var value = Math.floor((Math.random()*6)+1); // Model
-      $(dieHtml).text(value); // Vista
-    });
-  });
-});
+//   // Event listener
+//   $('#roller button.roll').on('click', function() { // Controlador
+//     $('.die').each(function(index, dieHtml) { // Controlador
+//       var value = Math.floor((Math.random()*6)+1); // Model
+//       $(dieHtml).text(value); // Vista
+//     });
+//   });
+// });
+
+function Die(sides){
+  this.sides  = sides
+  this.value = 0
+}
+
+
+Die.prototype.roll = function(){
+  this.value = Math.floor((Math.random()*this.sides)+1)
+}
+
+function View(){
+
+}
+
+
+View.prototype.changeDieValue = function(dieHtml, value){
+  $(dieHtml).text(value)  
+}
+
+
+View.prototype.paintNewDice = function(event) { 
+    $('.dice').append('<div class="die">0</div>') // Vista
+}
+
+function Controller(model, view){
+  this.model = model
+  this.view =  view
+}
+
+Controller.prototype.addCreateDieListener = function() {
+  $('#roller button.add').on('click', this.view.paintNewDice)
+}
+
+Controller.prototype.addRollDieListener = function(){
+  var controller = this
+  $('#roller button.roll').on('click', function(event){
+    $('.die').each(function(index, dieHtml) {
+      console.log(this, controller)
+      controller.model.roll()
+      var newValue = controller.model.value
+      controller.view.changeDieValue(dieHtml, newValue)
+    })
+  })
+}
+
+die = new Die(6)
+var view = new View()
+var controller = new Controller (die, view)
+controller.addCreateDieListener()
+controller.addRollDieListener()
+// dieHtml =  $(".die").first()
+// view.changeDieValue(dieHtml, die.value)
+// die.roll
+// controller.view
+// controller.model
+
+
+
+
+
+// Controller.prototype.listenCreateDie = function() {
+//   $('#roller button.add').on('click', this.view.paintNewDice)
+// }
+
+
+// Controller.prototype.addValueToDice = function(index, dieHtml) { // Controlador
+//       var value = Math.floor((Math.random()*6)+1); // Model
+//       $(dieHtml).text(value); // Vista
+//     }
+
+// function Controller(model, view){
+//   this.model = model
+//   this.view = view
+// }
+
+// var play = new Controller (new Die(), new View())
+// play = {model: {}, view: {}}
+// play.listen_create_die()
+// play.prototype.listen_create_die()
+
+//controller con prototype
+
 
 
--- challenges/4-garden/garden.js
+++ challenges/4-garden/garden.js
@@ -3,11 +3,84 @@
 //------------------------------------------------------------------------------------------------------------------
 // TU CODIGO: Crea un Objeto literal "garden", y funcion constuctora y prototipos para Flower aca.
 //------------------------------------------------------------------------------------------------------------------
-
-// var Garden = {
-//   name: "Kula Botanical Garden",
-//   this.win = function(){}
+// function Student(name, age) {
+//   Person.call(this, 'hispanic', 2443)
+//   this.name = name
+//   this.age = age
 // }
+
+// Student.prototype.speak = function() {
+//   console.log(`Hola soy ${this.name}`)
+// }
+
+// // Prototype chain
+
+// juan = new Student('juan', 43)
+// juan.speak()
+// juan.eat()
+
+function Flower(name,color){
+  this.name = name
+  this.color = color
+//   this.identify = function(){
+//  return `I am an ${this.name} and I am ${this.color}.`
+// }
+
+}
+
+
+
+Flower.prototype.identify =  function(){
+ return `I am an ${this.name} and I am ${this.color}.`
+}
+
+
+
+
+//   flowers[0].identify() === "I am an Aster and I am red.", "asters are red"
+//   );
+// assert(
+//   flowers[2].name === "Daffodil", "expected 'Daffodil'"
+//   );
+// assert(
+//   flowers[0].identify === flowers[5].identify, "only one implementation of the identify() function should exist"
+//   );
+
+
+garden = {
+  name: "Kula Botanical Garden",
+  location: "Makawao",
+  flowers: ["a", "b"],
+  color: "no tengo",
+  plant: function(flowers){
+    // console.log(`this flores ${this.flowers}`)
+    // console.log(`flowers ${flowers}`)
+    this.flowers = flowers.concat(this.flowers)
+  },
+  selectByColor: function(colour){
+    var res = []
+    this.flowers.forEach(function(elem){
+      if (elem.color === colour){
+        res.push(elem)
+      }
+    })
+    //console.log(res)
+    return res
+  },
+
+  selectByName: function(name){
+    var res = []
+    this.flowers.forEach(function(elem){
+      if (elem.name === name){
+        res.push(elem)
+      }
+    })
+    //console.log(res)
+    return res
+  }
+
+}
+
 
 
 
@@ -23,38 +96,39 @@ function assert(test, message) {
 }
 
 var flowers = [
-  new Flower("Aster", "red"),
-  new Flower("Bird of Paradise", "orange"),
-  new Flower("Daffodil", "yellow"),
-  new Flower("Gladiolus", "pink"),
-  new Flower("Iris", "purple"),
-  new Flower("Narcissus", "white"),
-  new Flower("Chrysanthemum", "yellow"),
+new Flower("Aster", "red"),
+new Flower("Bird of Paradise", "orange"),
+new Flower("Daffodil", "yellow"),
+new Flower("Gladiolus", "pink"),
+new Flower("Iris", "purple"),
+new Flower("Narcissus", "white"),
+new Flower("Chrysanthemum", "yellow"),
 ];
+
 
 garden.plant(flowers);
 
 
 assert(
   garden.name === "Kula Botanical Garden", "the garden has a name"
-);
+  );
 assert(
   garden.location === "Makawao", "the garden is located in Maui"
-);
+  );
 assert(
   garden.selectByColor("yellow").length === 2, "the garden should have 2 yellow flowers"
-);
+  );
 assert(
   garden.selectByName("Iris").length === 1, "the garden should have 1 Iris"
-);
+  );
 assert(
   flowers[0].identify() === "I am an Aster and I am red.", "asters are red"
-);
+  );
 assert(
   flowers[2].name === "Daffodil", "expected 'Daffodil'"
-);
+  );
 assert(
   flowers[0].identify === flowers[5].identify, "only one implementation of the identify() function should exist"
-);
+  );
 
 console.log("Terminaste todo funciona");
